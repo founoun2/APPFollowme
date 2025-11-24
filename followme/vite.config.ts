021d3d6ee57f0e6522a1c4ad62b48604e0f54c41
Mohamed Founoun <founoun.mohamed1@gmail.com>
2025-11-24 07:17:36 +0100

    Optimize code-splitting and update firebase.json for dist deployment

--- followme/vite.config.ts
+++ followme/vite.config.ts
@@ -18,6 +18,21 @@ export default defineConfig(({ mode }) => {
         alias: {
           '@': path.resolve(__dirname, '.'),
         }
-      }
+      },
+      build: {
+        rollupOptions: {
+          output: {
+            manualChunks(id) {
+              if (id.includes('node_modules')) {
+                return id
+                  .toString()
+                  .split('node_modules/')[1]
+                  .split('/')[0]
+                  .toString();
+              }
+            },
+          },
+        },
+      },
     };
 });
